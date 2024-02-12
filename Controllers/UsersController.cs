6beb17a60c58d478ae8d69f35723cb302ccf7637
kananbayramli <bayramli-kenan@mail.ru>
2024-02-12 22:23:51 +0400

    Create User [post/form]

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using IdentityApp.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,5 +15,33 @@ public class UsersController : Controller
     public IActionResult Index()
     {
         return View(_userManager.Users);
+    }
+
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    public async  Task<IActionResult> Create(CreateViewModel model)
+    {
+        if(ModelState.IsValid)
+        {
+            var user = new IdentityUser{UserName = model.UserName, Email = model.Email};
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            foreach(IdentityError err in result.Errors)
+            {
+                ModelState.AddModelError("", err.Description);
+            }
+        }
+        return View(model);
     }
 }
---
+++ ViewModels/CreateViewModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.ViewModels;
+
+public class CreateViewModel
+{
+    [Required]
+    public string UserName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password), ErrorMessage ="Parol duzgun gelmir...")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
