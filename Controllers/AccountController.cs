55a81f0d65c44af9c9f6002960e1e2a986575e0a
kananbayramli <bayramli-kenan@mail.ru>
2024-02-15 17:09:08 +0400

    Profile confirm link to login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -40,6 +40,12 @@ public class AccountController : Controller
             {
                 await _signInManager.SignOutAsync();
 
+                if(!await _userManager.IsEmailConfirmedAsync(user))
+                {
+                    ModelState.AddModelError("", "Hesabinizi tesdiq edin !");
+                    return View(model);
+                }
+
                 var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, true);
 
                 if(result.Succeeded)
@@ -66,4 +72,64 @@ public class AccountController : Controller
 
         return View(model);
     }
+
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    public async  Task<IActionResult> Create(CreateViewModel model)
+    {
+        if(ModelState.IsValid)
+        {
+            var user = new AppUser{UserName = model.UserName, Email = model.Email, FullName= model.FullName};
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+            if(result.Succeeded)
+            {
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var url = Url.Action("ConfirmEmail","Account", new {user.Id, token});
+
+                // email
+                TempData["message"] = "Email hesabinizdaki tesdiq mailini klikleyin";
+                return RedirectToAction("Login", "Account");
+            }
+
+            foreach(IdentityError err in result.Errors)
+            {
+                ModelState.AddModelError("", err.Description);
+            }
+        }
+        return View(model);
+    }
+
+
+    public async Task<IActionResult> ConfirmEmail(string Id, string token)
+    {
+        if(Id == null || token == null)
+        {
+            TempData["message"] = "Uygun olmayan token";
+            return View();
+        }
+
+        var user = await _userManager.FindByIdAsync(Id);
+        
+        if(user != null)
+        {
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+
+            if(result.Succeeded)
+            {
+                TempData["message"] = "Hesabiniz tesdiq edilmishdir!";
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        TempData["message"] = "Bele bir user tapilmadi!";
+        return View();
+    }
+
 }
--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -24,35 +24,6 @@ public class UsersController : Controller
     }
 
 
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-
-    [HttpPost]
-    public async  Task<IActionResult> Create(CreateViewModel model)
-    {
-        if(ModelState.IsValid)
-        {
-            var user = new AppUser{UserName = model.UserName, Email = model.Email, FullName= model.FullName};
-            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
-
-            if(result.Succeeded)
-            {
-                return RedirectToAction("Index");
-            }
-
-            foreach(IdentityError err in result.Errors)
-            {
-                ModelState.AddModelError("", err.Description);
-            }
-        }
-        return View(model);
-    }
-
-
-
 
     public async Task<IActionResult> Edit(string id)
     {
